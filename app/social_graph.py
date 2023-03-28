6ad2f1fb426f038de0b2ffcdbd5ba0133e278256
Amaan-N-K <amaan.n.khan.0102@gmail.com>
2023-03-28 16:24:32 -0400

    changed to id

--- app/social_graph.py
+++ app/social_graph.py
@@ -13,11 +13,13 @@ class _User:
     vertex of the graph representing each user
     """
     item: User
+    user_id: int
     matches: set[_User]
 
     def __init__(self, item: User, matches: set[_User]):
         self.item = item
         self.matches = matches
+        self.user_id = item.id
 
 
 @check_contracts
@@ -25,7 +27,7 @@ class Network:
     """
     The graph representing the network of matches betwen users
     """
-    _users: dict[User, _User]
+    _users: dict[int, _User]
 
     def __init__(self) -> None:
         """
@@ -40,7 +42,7 @@ class Network:
         Preconditions:
             - user not in self._users
         """
-        self._users[user] = _User(user, set())
+        self._users[user.id] = _User(user, set())
 
     def add_connection(self, user1: User, user2: User) -> None:
         """
@@ -49,9 +51,9 @@ class Network:
         Preconditions:
             - user1 != user2
         """
-        if user1 in self._users and user2 in self._users:
-            u1 = self._users[user1]
-            u2 = self._users[user2]
+        if user1.id in self._users and user2.id in self._users:
+            u1 = self._users[user1.id]
+            u2 = self._users[user2.id]
 
             u1.matches.add(u2)
             u2.matches.add(u1)
@@ -62,27 +64,27 @@ class Network:
         """
         check whether 2 users have a connection/ matched
         """
-        if user1 in self._users and user2 in self._users:
-            u1 = self._users[user1]
-            return any(u2.item == user2 for u2 in u1.matches)
+        if user1.id in self._users and user2.id in self._users:
+            u1 = self._users[user1.id]
+            return any(u2.user_id == user2.id for u2 in u1.matches)
         else:
             return False
 
-    def get_matches(self, user: User) -> set:
+    def get_matches(self, user: User) -> set[int]:
         """
-        return a set of matches of the give user
+        return a set of user ids for the matches of the given user
         """
-        if user in self._users:
-            u = self._users[user]
-            return {match.user for match in u.matches}
+        if user.id in self._users:
+            u = self._users[user.id]
+            return {match.user_id for match in u.matches}
         else:
             raise ValueError
 
 
-def create_network(matches: list[dict[User, set[User]]]) -> Network:
+def create_network(matches: list[dict[int, set[User]]]) -> Network:
     """
     create a network from matches
     """
-    my_network = Network
+    my_network = Network()
     for match in matches:
         ...
